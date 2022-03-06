71403b565d0d3981b08e6aea746409bdfe3f5296
= <ayushsingla909@gmail.com>
2022-03-06 15:29:13 +0530

    Implemented Context API & useReducer for Display & Clear Alert

---
+++ client/src/context/appContext.js
@@ -0,0 +1,42 @@
+import React, { useReducer, useContext } from "react"
+import { DISPLAY_ALERT, CLEAR_ALERT } from "./action"
+import reducer from "./reducer"
+
+const initialState = {
+  isLoading: false,
+  showAlert: false,
+  alertText: "",
+  alertType: "",
+}
+
+const AppContext = React.createContext()
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const displayAlert = () => {
+    dispatch({
+      type: DISPLAY_ALERT,
+    })
+    clearAlert()
+  }
+
+  const clearAlert = () => {
+    setTimeout(() => {
+      dispatch({
+        type: CLEAR_ALERT,
+      })
+    }, 3000)
+  }
+  return (
+    <AppContext.Provider value={{ ...state, displayAlert }}>
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+//make your own hook
+const useAppContext = () => {
+  return useContext(AppContext)
+}
+
+export { AppProvider, initialState, useAppContext }
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react"
 import { Alert, FormRow, Logo } from "../components"
 import Wrapper from "../assets/wrappers/RegisterPage"
+import { useAppContext } from "../context/appContext"
 
 const initialState = {
   name: "",
   email: "",
   password: "",
   isMember: true,
-  showAlert: false,
 }
 
 const Register = () => {
   const [values, setValues] = useState(initialState)
+  const { isLoading, showAlert, displayAlert } = useAppContext()
 
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember })
@@ -19,18 +20,24 @@ const Register = () => {
 
   const handleChange = (e) => {
     console.log(e.target)
+    setValues({ ...values, [e.target.name]: e.target.value })
   }
 
   const onSubmit = (e) => {
     e.preventDefault()
-    console.log(e.target)
+    const { name, email, password, isMember } = values
+    if (!email || !password || (!isMember && !name)) {
+      displayAlert()
+      return
+    }
+    console.log(values)
   }
   return (
     <Wrapper className='full-page'>
       <form className='form' onSubmit={onSubmit}>
         <Logo />
         <h3>{values.isMember ? "Login" : "Register"}</h3>
-        {values.showAlert && <Alert />}
+        {showAlert && <Alert />}
         {/* name input */}
         {/* if isMember is false (not true) then display name input field */}
         {!values.isMember && (
@@ -56,11 +63,13 @@ const Register = () => {
           value={values.password}
           handleChange={handleChange}
         />
-        <button type='submit' className='btn btn-block'></button>
+        <button type='submit' className='btn btn-block' disabled={isLoading}>
+          Submit
+        </button>
         <p>
           {values.isMember ? "Not a member yet?" : "Already a member?"}
           <button type='button' onClick={toggleMember} className='member-btn'>
-            {values.isMember ? "Login" : "Register"}
+            {values.isMember ? "Register" : "Login"}
           </button>
         </p>
       </form>
