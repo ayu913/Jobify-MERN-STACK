19a8e817a8e8f71fbe72ce75b74a828b1e8790d3
= <ayushsingla909@gmail.com>
2022-03-22 01:42:44 +0530

    server error solved

--- server/server.js
+++ server/server.js
@@ -27,9 +27,6 @@ import authenticateUser from "./middleware/auth.js"
 if (process.env.NODE_ENV != "production") {
   app.use(morgan("dev"))
 }
-const __dirname = dirname(fileURLToPath(import.meta.url))
-// only when ready to deploy
-app.use(express.static(path.resolve(__dirname, "./client/build")))
 
 app.use(cors())
 app.use(express.json())
@@ -40,10 +37,17 @@ app.use(mongoSanitize())
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/jobs", authenticateUser, jobsRouter)
 
-// only when ready to deploy
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"))
-})
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")))
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  )
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....")
+  })
+}
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
