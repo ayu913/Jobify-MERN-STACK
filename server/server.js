545e8c8e4627cac463c4c806f58e38d220769446
= <ayushsingla909@gmail.com>
2022-03-22 01:47:21 +0530

    server error solved

--- server/server.js
+++ server/server.js
@@ -37,6 +37,7 @@ app.use(mongoSanitize())
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/jobs", authenticateUser, jobsRouter)
 
+const __dirname = path.resolve()
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")))
 
