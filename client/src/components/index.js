2d8c5cdf4018e30749b277003af9971ac908237d
= <ayushsingla909@gmail.com>
2022-03-22 00:41:09 +0530

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,27 @@
+import Logo from "./Logo"
+import FormRow from "./FormRow"
+import Alert from "./Alert"
+import Navbar from "./Navbar"
+import BigSidebar from "./BigSidebar"
+import SmallSidebar from "./SmallSidebar"
+import SearchContainer from "./SearchContainer"
+import JobsContainer from "./JobsContainer"
+import StatsContainer from "./StatsContainer"
+import StatItem from "./StatItem"
+import ChartsContainer from "./ChartsContainer"
+import Loading from "./Loading"
+
+export {
+  Navbar,
+  BigSidebar,
+  SmallSidebar,
+  Logo,
+  FormRow,
+  Alert,
+  SearchContainer,
+  JobsContainer,
+  StatsContainer,
+  StatItem,
+  ChartsContainer,
+  Loading,
+}
